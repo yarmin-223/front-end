17a9f19db375541e5eba46760cad7f4ff193e9c6
jesus melendez <jesus.melendez@theappmaster.com>
2024-02-21 19:20:50 -0300

    optimizacion de codigo

--- wedly/js/scripts.js
+++ wedly/js/scripts.js
@@ -2,21 +2,26 @@
 
 
 /******  YARMIN ******/
-
+/*** agrego este codigo javascript porque en la user history dice que se debe poder generar una reserva desde el boton booking aunque en la tarea solo dice de agregar el boton me parecio necesario agregar este detalle  ***/
 function mostrarDivTurno() {
-    
-    document.getElementById("divTurno").style.display = "block";
+  
+    document.getElementById("DivModalReserva").style.display = "block";
   }
 
   function confirmarTurno() {
     
-    var fechaSeleccionada = document.getElementById("fechaInput").value;
+    const controlfecha = document.getElementById("fechaInput");
 
-    
-    alert("Su reserva ha sido confirmada para la fecha: " + fechaSeleccionada);
+    const fechaelegida = controlfecha.value;
 
+    /** aqui deberia ir alguna logica para la fecha pero como hay mucho detalle dejo fijo solo un mensajito con la fecha seleccionada ***/
     
-    document.getElementById("divTurno").style.display = "none";
+    alert("Su reserva ha sido confirmada para la fecha: " + fechaelegida);
+
+    const divmodal = document.getElementById("DivModalReserva");
+
+
+    divmodal.style.display = "none";
   }
 
   /******  YARMIN ******/
