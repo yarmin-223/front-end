7c8c102c15117cba4a9f59597d668ac9ab75a7f0
Yar_min <oropezayarmin@gmail.com>
2024-02-27 15:26:28 -0300

    agregado de proyecto completo

--- wedly/js/scripts.js
+++ wedly/js/scripts.js
@@ -3,25 +3,44 @@
 
 /******  YARMIN ******/
 /*** agrego este codigo javascript porque en la user history dice que se debe poder generar una reserva desde el boton booking aunque en la tarea solo dice de agregar el boton me parecio necesario agregar este detalle  ***/
-function mostrarDivTurno() {
+// function mostrarDivTurno() {
   
-    document.getElementById("DivModalReserva").style.display = "block";
-  }
+//     document.getElementById("DivModalReserva").style.display = "block";
+//   }
 
-  function confirmarTurno() {
+//   function confirmarTurno() {
     
-    const controlfecha = document.getElementById("fechaInput");
+//     const controlfecha = document.getElementById("fechaInput");
 
-    const fechaelegida = controlfecha.value;
+//     const fechaelegida = controlfecha.value;
 
-    /** aqui deberia ir alguna logica para la fecha pero como hay mucho detalle dejo fijo solo un mensajito con la fecha seleccionada ***/
+//     /** aqui deberia ir alguna logica para la fecha pero como hay mucho detalle dejo fijo solo un mensajito con la fecha seleccionada ***/
     
-    alert("Su reserva ha sido confirmada para la fecha: " + fechaelegida);
+//     alert("Su reserva ha sido confirmada para la fecha: " + fechaelegida);
 
-    const divmodal = document.getElementById("DivModalReserva");
+//     const divmodal = document.getElementById("DivModalReserva");
 
 
-    divmodal.style.display = "none";
-  }
+//     divmodal.style.display = "none";
+//   }
+
+//   document.addEventListener("DOMContentLoaded", function () {
+//     var summary = document.querySelector("details summary");
+//     var imgMenuCerrado = document.getElementById("imgMenuMobileCerrado");
+//     var imgMenuAbierto = document.getElementById("imgMenuMobileAbierto");
+
+//     summary.addEventListener("click", function () {
+//         // Verifica el estado actual del menú y ajusta la visibilidad de las imágenes en consecuencia
+//         if (imgMenuCerrado.style.display === "none") {
+//             // Si el menú está abierto, ciérralo y muestra la imagen del menú cerrado
+//             imgMenuCerrado.style.display = "inline";
+//             imgMenuAbierto.style.display = "none";
+//         } else {
+//             // Si el menú está cerrado, ábrelo y muestra la X
+//             imgMenuCerrado.style.display = "none";
+//             imgMenuAbierto.style.display = "inline";
+//         }
+//     });
+// });
 
   /******  YARMIN ******/
